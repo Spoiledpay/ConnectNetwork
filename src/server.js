6f3f7d54d1cd3fe9f35268835aaad746762c37c4
Spoiledpay <55815349+Spoiledpay@users.noreply.github.com>
2020-08-25 11:40:33 -0300

    primeiro commit

---
+++ src/server.js
@@ -0,0 +1,57 @@
+const app = require('express')();
+const bodyParser = require('body-parser');
+const httpServer = require('http').Server(app);
+const axios = require('axios');
+const io = require('socket.io')(httpServer);
+const client = require('socket.io-client');
+
+const BlockChain = require('./models/chain');
+const SocketActions  = require('./constants');
+
+const socketListeners = require('./socketListeners');
+
+const { PORT } = process.env;
+
+const blockChain = new BlockChain(null, io);
+
+app.use(bodyParser.json());
+
+app.post('/nodes', (req, res) => {
+  const { host, port } = req.body;
+  const { callback } = req.query;
+  const node = `http://${host}:${port}`;
+  const socketNode = socketListeners(client(node), blockChain);
+  blockChain.addNode(socketNode, blockChain);
+  if (callback === 'true') {
+    console.info(`Node Adicionado ${node} back`);
+    res.json({ status: 'Added node Back' }).end();
+  } else {
+    axios.post(`${node}/nodes?callback=true`, {
+      host: req.hostname,
+      port: PORT,
+    });
+    console.info(`Node Adicionado ${node}`);
+    res.json({ status: 'Node Adicionado' }).end();
+  }
+});
+
+app.post('/transaction', (req, res) => {
+  const { sender, receiver, amount } = req.body;
+  io.emit(SocketActions.ADD_TRANSACTION, sender, receiver, amount);
+  res.json({ message: 'sucesso da transação' }).end();
+});
+
+app.get('/chain', (req, res) => {
+  res.json(blockChain.toArray()).end();
+});
+
+io.on('conexão', (socket) => {
+  console.info(`Socket connected, ID: ${socket.id}`);
+  socket.on('disconnect', () => {
+    console.log(`Socket disconnected, ID: ${socket.id}`);
+  });
+});
+
+blockChain.addNode(socketListeners(client(`http://localhost:${PORT}`), blockChain));
+
+httpServer.listen(PORT, () => console.info(`Express server running on ${PORT}...`));
